4e99b815dd927cd8be8ec78dcc1b1b3c0e873ae3
Tshepiso <timarape.gmail.com>
2020-11-18 17:28:27 +0200

    Initial Commit

---
+++ Fundamentals/app.js
@@ -0,0 +1,67 @@
+// console.log('This is a string');
+// console.log(23424234);
+// console.log(true);
+
+// var somethigElse = "We are here to stay";
+// console.log(somethigElse);
+// console.log([1,2,3,4,5,6,7]);
+
+// console.log({obj1:1, obj2:2});
+// console.table({obj1:1,obj2:2});
+
+// var name = 'Tshepiso';
+// console.log(name);
+// name ='Lesedi';
+// console.log(name);
+
+// var firstName = 'John'
+
+// let name = 'Tshepiso';
+// console.log(name);
+// name ='Lesedi';
+// console.log(name);
+
+// var firstName = 'John'
+
+// const name = 'Tshepiso';
+// console.log(name);
+
+
+//Primitive
+
+// let name = 'Tshepiso';
+
+// console.log(typeof(name));
+
+// let value;
+
+// value = 10;
+
+// value =(5).toString();
+
+
+
+// console.log(value);
+// console.log(typeof(value))
+// //console.log(value.length);
+
+
+
+const firstName = 'Tshepiso';
+const lastName = 'Marape';
+const age = 32;
+
+let value;
+
+
+value = (firstName + ' '+ lastName).toUpperCase();
+
+console.log(value);
+
+value = GoodBye();
+console.log(value)
+
+function GoodBye()
+{
+    return "Tatatatatata";
+}
