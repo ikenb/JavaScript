1bd8f854960157b1f1e8b16057e19cefdd188406
Tshepiso <timarape.gmail.com>
2020-12-03 22:14:13 +0200

    Added Expanding Cards

--- Fundamentals/app.js
+++ Fundamentals/app.js
@@ -1,67 +1,12 @@
-// console.log('This is a string');
-// console.log(23424234);
-// console.log(true);
+//Log onto the console
+console.log('This will be fun');
 
-// var somethigElse = "We are here to stay";
-// console.log(somethigElse);
-// console.log([1,2,3,4,5,6,7]);
+console.log(121313);
 
-// console.log({obj1:1, obj2:2});
-// console.table({obj1:1,obj2:2});
+var name ="Tshepiso";
 
-// var name = 'Tshepiso';
-// console.log(name);
-// name ='Lesedi';
-// console.log(name);
+console.log(name);
 
-// var firstName = 'John'
+console.error('This is the error');
 
-// let name = 'Tshepiso';
-// console.log(name);
-// name ='Lesedi';
-// console.log(name);
-
-// var firstName = 'John'
-
-// const name = 'Tshepiso';
-// console.log(name);
-
-
-//Primitive
-
-// let name = 'Tshepiso';
-
-// console.log(typeof(name));
-
-// let value;
-
-// value = 10;
-
-// value =(5).toString();
-
-
-
-// console.log(value);
-// console.log(typeof(value))
-// //console.log(value.length);
-
-
-
-const firstName = 'Tshepiso';
-const lastName = 'Marape';
-const age = 32;
-
-let value;
-
-
-value = (firstName + ' '+ lastName).toUpperCase();
-
-console.log(value);
-
-value = GoodBye();
-console.log(value)
-
-function GoodBye()
-{
-    return "Tatatatatata";
-}
+console.warn('This is a wertsfsfv');
